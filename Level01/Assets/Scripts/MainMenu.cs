d8b1051c3ffb7ba4ba5b93c37c82d2bf3dfffb01
Leigh Salvador <lssalvador@csu.fullerton.edu>
2018-03-04 22:08:08 -0800

    added Main Menu - Play

---
+++ Level01/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //loads scenes
+
+public class MainMenu : MonoBehaviour {
+
+	// Play button clicked
+	public void Play () {
+		//loads next scene in the queue
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
